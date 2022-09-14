83b7c0145d90b5ee40dc329c4c83a66e18d497a9
jonathan <fonggx-wm19@student.tarc.edu.my>
2022-09-15 05:44:33 +0800

    12:49 Merged 1 04.30 Merged(Changed into Kotlin File)

---
+++ app/src/main/java/com/example/ofinsurance/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.ofinsurance
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_owned, container, false)
+
+
+
+
+
+        return v
+    }
+
+
+}
