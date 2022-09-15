ab48b041bc1718d8a2cc5b433566b1e700d1a1a6
jonathan <fonggx-wm19@student.tarc.edu.my>
2022-09-15 15:36:04 +0800

    12:49 Merged 1 04.30 Merged(Changed into Kotlin File) 03:35 Merged(payment)

--- app/src/main/java/com/example/ofinsurance/OwnedFragment.kt
+++ app/src/main/java/com/example/ofinsurance/OwnedFragment.kt
@@ -7,26 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 
 class OwnedFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = inflater.inflate(R.layout.fragment_owned, container, false)
-
-
-
-
-
-        return v
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_owned, container, false)
+        val pay : Button = view.findViewById(R.id.paybtn1)
+        pay.setOnClickListener{
+            val fragment = OwnedBillFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.own,fragment)?.commit()
+        }
+        return view
     }
-
-
 }
